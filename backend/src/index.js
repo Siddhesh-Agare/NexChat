b1c79fe21d0a5e1714bfe4d46204715d73e4dd69
Siddhesh Agare <siddheshagare432@gmail.com>
2026-07-31 14:06:40 +0530

    fix: correct import name for Clerk webhooks

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import fs from 'fs'
 import path from 'path'
 import job from './lib/cron.js'
 
-import clerkWebhook from './webhooks/clerk.webhook.js'
+import clerkWebhooks from './webhooks/clerk.webhook.js'
 
 const app = express()
 const PORT = process.env.PORT
